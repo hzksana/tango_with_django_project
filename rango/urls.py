8c96097b72b030a8068e6accd4b2df90ba97a636
Rei Mito <2876372m@student.gla.ac.uk>
2025-01-31 01:11:10 +0900

    changes made to pass test chapter3

--- rango/urls.py
+++ rango/urls.py
@@ -4,4 +4,7 @@ from rango import views
 app_name = 'rango'
 urlpatterns = [
 path('', views.index, name='index'),
+path('about/', views.about, name='about'),
+
 ]
+# URL mappings can employ reverse URL matching. That is we can reference the URL mapping by name rather than by the URL
