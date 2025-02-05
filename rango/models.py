7970b6cc9ab5cce2831644934eabbcc637ee61c4
Rei Mito <2876372m@student.gla.ac.uk>
2025-02-05 23:29:18 +0900

    5 passed

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+    #including __str__() as defined ,  will see <Category: Python> instead of <Category: Category object> if you were to print() the object 
+    
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)# foreign key, can create one to many relationship with model/table category
+    title = models.CharField(max_length=128)#store character data
+    url = models.URLField()#store resource URL
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -13,9 +13,20 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates') # dir for templates
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -53,7 +64,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 #<workspace>/tango_with_django_project/templates/
 
 TEMPLATES = [
@@ -67,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
