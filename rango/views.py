436d0f5f61e8f9bf249ed5c1814082e273e5cdde
Rei Mito <2876372m@student.gla.ac.uk>
2025-02-11 22:56:25 +0900

    6 test passed

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rango.models import Page
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 # Create your views here.
 
 def show_category(request, category_name_slug):
@@ -39,10 +39,14 @@ def index(request):
     # Place the list in our context_dict dictionary (with our boldmessage!)
     # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    
+    page_list = Page.objects.order_by('-views')[:5]
+
+
     context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, !'
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
     
     # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
